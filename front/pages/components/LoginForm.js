80fab462f674840416a9d0024033091f3c67173f
Jennfier Heejin Kang <39927897+HKang0302@users.noreply.github.com>
2022-01-23 00:06:39 +0900

    started nodebird project - ended rerendering part

---
+++ front/pages/components/LoginForm.js
@@ -0,0 +1,53 @@
+import React, { useCallback, useState, useMemo } from "react";
+import { Button, Form, Input } from "antd";
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+    margin-top: 10px
+`;
+
+const LoginForm = () => {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onChangeId = useCallback((e)=> {
+        setId(e.target.value);
+    }, []);
+
+    const onChangePassword = useCallback((e)=> {
+        setPassword(e.target.value);
+    }, []);
+
+    const style = useMemo(()=> ({marginTop:10}), []);
+
+    return(
+        <Form>
+            <div>
+                <label htmlFor="user-id">아이디</label>
+                <br/>
+                <Input name="user-id" value={id} onChange={onChangeId} required />
+            </div>
+            <div>
+                <label htmlFor="user-password">패스워드</label>
+                <br/>
+                <Input 
+                    name="user-password" 
+                    type="password"
+                    value={password} 
+                    onChange={onChangePassword} 
+                    required 
+                />
+            </div>
+            <ButtonWrapper style={style}>
+                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Link href='/signup'><a><Button>회원가입</Button></a></Link>
+            </ButtonWrapper>
+            <div>
+
+            </div>
+        </Form>
+    );
+}
+
+export default LoginForm;
---
+++ front/pages/components/UserProfile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Form } from "antd";
+
+const UserProfile = () => {
+    return(
+        <div></div>
+    );
+}
+
+export default UserProfile;
